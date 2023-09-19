008c00db4e20fe0b41eb32fcbb4d08eead2a846c
KongCPH <112859698+KongCPH@users.noreply.github.com>
2023-09-19 12:33:12 +0200

    Update sportsclub_test.sql

--- src/main/resources/sportsclub_test.sql
+++ src/main/resources/sportsclub_test.sql
@@ -1,12 +1,51 @@
 /*
     This sql script should only be executed once to create table structures for the test database.
-    It copies the table structures from the 5 tables in the original database (sportsclub)
+    It replicates the table structures from the 5 tables in the original database (sportsclub)
     1) First create a new database called "sportsclub_test" 
     2) Run this script
 */
+-- Table structure for table zip
+CREATE TABLE zip (
+  zip int PRIMARY KEY,
+  city varchar(45) NOT NULL
+);
 
-CREATE TABLE sportsclub_test.member LIKE sportsclub.member;
-CREATE TABLE sportsclub_test.zip LIKE sportsclub.zip;
-CREATE TABLE sportsclub_test.registration LIKE sportsclub.registration;
-CREATE TABLE sportsclub_test.sport LIKE sportsclub.sport;
-CREATE TABLE sportsclub_test.team LIKE sportsclub.team;
+
+-- Table structure for table sport
+CREATE TABLE sport (
+  sport_id serial PRIMARY KEY,
+  sport varchar(45) NOT NULL
+);
+
+
+-- Table structure for table team
+CREATE TABLE team (
+  team_id varchar(10) NOT NULL,
+  sport_id int NOT NULL,
+  PRIMARY KEY (team_id),
+  CONSTRAINT fk_team_sport1 FOREIGN KEY (sport_id) REFERENCES sport (sport_id)
+);
+
+
+-- Table structure for table member
+CREATE TABLE member (
+  member_id serial PRIMARY KEY,
+  name varchar(45),
+  address varchar(45),
+  zip int,
+  gender varchar(1),
+  year varchar(45),
+  CONSTRAINT fk_member_zip FOREIGN KEY (zip) REFERENCES zip (zip)
+);
+
+-- Table structure for table registration
+CREATE TABLE registration (
+  member_id int NOT NULL,
+  team_id varchar(10) NOT NULL,
+  price int,
+  PRIMARY KEY (member_id, team_id),
+  CONSTRAINT fk_registration_member1 FOREIGN KEY (member_id) REFERENCES member (member_id),
+  CONSTRAINT fk_registration_team1 FOREIGN KEY (team_id) REFERENCES team (team_id)
+);
+
+
